1d759e1ffd16ec75367d3cb51a8d0bc1ca32c42f
imperative-scientist-4600 <nelom3@gmail.com>
2021-06-10 13:59:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,8 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
  
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+url = "https://flatironschool.com/"
+binding.pry
+doc = Nokogiri::HTML(open(url))
 doc.css(".headline-26OIBN")
